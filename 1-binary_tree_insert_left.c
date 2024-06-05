18e37fd7d6614058a3b5c9c6e71ac2032e8f6386
buyekeobare <buyekeobare@gmail.com>
2024-06-05 12:58:47 -0700

    Updated file

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_insert_left - Inserts the left child
- * @parent: Pointer to the node to insert the left child
+ * @parent: Pointer to the node to insert the left child in 
  * @value: Value to store in the new_left_child
  * Return: Pointer to the new_left_child || NULL on failure or parent is NULL
  * If parent already has a left child, replace with new_left_child node and set
--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,6 +4,8 @@
  * binary_tree_preorder - Pre-order travesal
  * @tree: Pointer to root node
  * @func: Pointer to a function to call for each node
+ * Description: Value of node must pass a param to this function
+ * If tree || func is NULL do nothing
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))

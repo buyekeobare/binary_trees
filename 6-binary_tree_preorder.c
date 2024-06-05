369d6d71ffbee2ef4d921e10cedff7d1cd014c3b
buyekeobare <buyekeobare@gmail.com>
2024-06-05 12:12:39 -0700

    Updated files

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,8 +4,6 @@
  * binary_tree_preorder - Pre-order travesal
  * @tree: Pointer to root node
  * @func: Pointer to a function to call for each node
- * Description: The value of the node must passed a param to this function
- * If tree or func is NULL do nothing
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
@@ -16,7 +14,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 
 	func(tree->n);
 
-	binary_tree_preorder(tree->right, func);
-
 	binary_tree_preorder(tree->left, func);
+
+	binary_tree_preorder(tree->right, func);
 }

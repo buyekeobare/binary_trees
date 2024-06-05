a3e41a8359b8c4e8fe77609bf465b77306fd3dfa
emmanueldanso1 <kwekudanso21@gmail.com>
2024-06-05 07:51:40 -0700

    checks parent->parent-> of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,10 +1,10 @@
 #include "binary.h"
 /**
-binary_tree_uncle - functions shows if tree has siblings
-@node: node of tree to check
-@Return: uncle node
+* binary_tree_uncle - functions shows if tree has siblings
+* @node: node of tree to check
+* Return: uncle node
 */
-binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL) return NULL;
 	if (node->parent->parent->left == node->parent)

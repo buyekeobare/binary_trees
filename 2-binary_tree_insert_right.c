20fc9fc6648ef810283a1948fcc41bdf433a36d8
buyekeobare <buyekeobare@gmail.com>
2024-06-05 11:08:32 -0700

    2-binary_tree_insert_right.c updated file

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,38 +1,37 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Insert the right child
- * @parent: Pointer to the node to insert the right child
- * @value: Value to store in the new_right_child node
- * Return: pointer to the right child || NULL on failure or parent is NULL
- * If parent already has a right child, replace with new_right_child node and set
+ * binary_tree_insert_right - insert right child
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in the new node
+ * Return: pointer to right child || NULL on failure || parent is NULL
+ * If parent already has a right-child, the new node must take its place
+ * and the old right-child must be set as the right-child of the new node.
  */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_right_child;
+	binary_tree_t *rightchild;
 
 	if (parent == NULL)
 		return (NULL);
 
-	new_right_child = malloc(sizeof(binary_tree_t));
+	rightchild = malloc(sizeof(binary_tree_t));
 
-	if (new_right_child == NULL)
+	if (rightchild == NULL)
 		return (NULL);
 
-	new_right_child->parent = parent;
-	new_right_child->n = value;
-	/* Replace the old right child, if it exists */
-	new_right_child->left = NULL;
-	new_right_child->right = parent->right;
-	/* Point right child parent to the new right child */
-	parent->right = new_right_child;
+	rightchild->parent = parent;
+	rightchild->n = value;
+	rightchild->left = NULL;
+	rightchild->right = parent->right;
 
-	/*If the old right child exists, set it as new right child */
-	if (new_right_child->right)
+	parent->right = rightchild;
+
+	if (rightchild->right)
 	{
-		new_right_child->right->parent = new_right_child;
+		rightchild->right->parent = rightchild;
 	}
 
-	return (new_right_child);
+	return (rightchild);
 }

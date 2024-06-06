82d7b8fae95a1146afc93b7853adbfe218ac70be
emmanueldanso1 <kwekudanso21@gmail.com>
2024-06-06 16:34:48 -0700

    18-updated

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,12 +1,13 @@
 #include "binary_trees.h"
 /**
-* binary_tree_uncle - functions shows if tree has siblings
-* @node: node of tree to check
-* Return: uncle node
+*binary_tree_uncle - functions shows if tree has siblings
+*@node: node of tree to check
+*Return: uncle node
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-if (node == NULL || node->parent == NULL || node->parent->parent == NULL) return NULL;
+if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+return NULL;
 if (node->parent->parent->left == node->parent)
 {
 return (node->parent->parent->right);

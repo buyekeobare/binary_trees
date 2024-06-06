0b69754c99986131de2173fb8899f210bb048cb3
emmanueldanso1 <kwekudanso21@gmail.com>
2024-06-06 04:20:13 -0700

    new update

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,14 +8,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaf = 0, l = 0, r = 0;
 	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		l = binary_trees_leaves(tree->left);
-		r = binary_trees_leaves(trees->right);
-		leaf = l + r;
-		return ((!l && !r) ? leaf + : leaf + 0);
-	}
+		return 0;
+	if (tree->left == NULL && tree->right == NULL)
+		return 1;
+	return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
 }

ca49048e3be39428eec748a832f0c3e24349366b
emmanueldanso1 <kwekudanso21@gmail.com>
2024-06-06 02:16:48 -0700

    return size of tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - functun returns the size of tree
+ * @tree: tree to be checked
+ * Return: size of tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0, lef = 0, r = 0;
+	if (tree == NULL) return (0);
+	else
+	{
+		lef = binary_tree_size(tree->left);
+		r = binary_tree_size(tree->right);
+		size = lef + r + 1;
+	}
+	return (size);
+}

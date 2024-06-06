27f371f3539c9953abe05817e4a0c6eaa325ad13
buyekeobare <buyekeobare@gmail.com>
2024-06-06 08:47:05 -0700

    100-binary_trees_ancestor.c Updated file

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,20 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - finds the lowest common ancestor
- * @first: pointer to the first node
- * @second: pointer to the second node
- * Return: pointer to the LCA || NULL on failure
+ * binary_trees_ancestor - Finds the lowest common ancestor (LCA)
+ * @first: Pointer to first node
+ * @second: Pointer to second node
+ * Return: Pointer to the LCA, or NULL no LCA
  */
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
-	const binary_tree_t *first_lca, *second_lca;
+	const binary_tree_t *left_ancestor, *right_ancestor;
 
-	for (first_lca = first; first_lca; first_lca = first_lca->parent)
-		for (second_lca = second; second_lca; second_lca = second_lca->parent)
-			if (first_lca == second_lca)
-				return ((binary_tree_t *)first_lca);
+	for (left_ancestor = first; left_ancestor; left_ancestor = left_ancestor->parent)
+		for (right_ancestor = second; right_ancestor; right_ancestor = right_ancestor->parent)
+			if (left_ancestor == right_ancetsor)
+				return ((binary_tree_t *)left_ancestor);
 	return (NULL);
 }

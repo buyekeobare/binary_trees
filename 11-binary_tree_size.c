a187df02009ef6295e59a41f3f13c939f3ef020e
buyekeobare <buyekeobare@gmail.com>
2024-06-06 15:22:32 -0700

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,12 +7,16 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size = 0, lef = 0, r = 0;
-	if (tree == NULL) return (0);
-	else
-	{
-		lef = binary_tree_size(tree->left);
-		r = binary_tree_size(tree->right);
-		size = lef + r + 1;
-	}
+
+	if (tree == NULL)
+		return (0);
+
+
+	lef = binary_tree_size(tree->left);
+	r = binary_tree_size(tree->right);
+	size = lef + r + 1;
+
 	return (size);
 }
+
+

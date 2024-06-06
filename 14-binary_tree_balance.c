af2aed209e935e8f957da655e8bc65389a19f813
buyekeobare <buyekeobare@gmail.com>
2024-06-06 16:51:00 -0700

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,19 +13,19 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	l_height = binary_tree_height_b(tree->left);
-	r_height = binary_tree_height_b(tree->right);
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
 
 	return (l_height - r_height);
 }
 
 /**
- * binary_tree_height_b - Measures height of a binary tree for a balanced tree
+ * binary_tree_height - Measures height of a binary tree for a balanced tree
  * @tree: tree to be checked
  * Return: Height of the tree, or 0 on failure
  */
 
-size_t binary_tree_height_b(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l_height = 0;
 	size_t r_height = 0;
@@ -33,8 +33,8 @@ size_t binary_tree_height_b(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	l_height += 1 + binary_tree_height_b(tree->left);
-	r_height += 1 + binary_tree_height_b(tree->right);
+	l_height += 1 + binary_tree_height(tree->left);
+	r_height += 1 + binary_tree_height(tree->right);
 
 	if (l_height > r_theight)
 		return (l_height);

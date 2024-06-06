db7cc8fc582ebaa616c10820400b8e26962942b2
buyekeobare <buyekeobare@gmail.com>
2024-06-06 09:43:00 -0700

    Updated file

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of the binary tree
- * @stem: pointer to measure the size of
- * Return: size of the tree || 0 if NULL
+ * binary_tree_size - counts the number of nodes in the binary tree
+ * @stem: pointer to the root node of the tree
+ * Return: Number of nodes in the binary tree
  */
 
 size_t binary_tree_size(const binary_tree_t *stem)

694d685ee56751559732fa3694576c1f555ffec9
buyekeobare <buyekeobare@gmail.com>
2024-06-06 17:01:28 -0700

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,7 +14,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	left_height = height_binary_tree(tree->left);
-	right_height = heigh_binary_tree(tree->right);
+	right_height = height_binary_tree(tree->right);
 
 	return (left_height - right_height);
 }

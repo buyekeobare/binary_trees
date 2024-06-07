5ed2d54691c3a1d89d65c3af92b8950c323d9e93
buyekeobare <buyekeobare@gmail.com>
2024-06-06 17:22:56 -0700

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,17 +6,12 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t left_leaves, right_leaves;
-
 	if (!tree)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
+	if (!tree->left && !tree->right)
 		return (1);
 
-	left_leaves = binary_tree_leaves(tree->left);
-	right_leaves = binary_tree_leaves(tree->right);
-
-	return (left_leaves + right_leaves);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
 
